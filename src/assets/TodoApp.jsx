52718fdd5eb224555acdab60b1e9d3990491b785
NiSHAN808 <nishan4b@gmail.com>
2025-02-22 23:47:05 +0545

    todays work

--- src/assets/TodoApp.jsx
+++ src/assets/TodoApp.jsx
@@ -1,56 +1,61 @@
-import React, { useState,useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './TodoApp.css';
 function TodoApp() {
-    let inputref=useRef(null);
+    let inputref = useRef(null);
 
-let [tasks,setTask]=useState([
-    {tak:"apple", status:true},
-    {tak:"banana", status:false},
-    {tak:"mango", status:true}
-]);
+    let [tasks, setTask] = useState([
+        { tak: "apple", status: true },
+        { tak: "banana", status: false },
+        { tak: "mango", status: true }
+    ]);
 
-function handleAddBtn(){
-    
-    let x= inputref.current.value;
-    let newtask={tak:x, status:true};
-    setTask(t=>[...t,newtask]);
-    inputref.current.value="";
-}
-function handleDelBtn(i){
-    let x=0;
-    setTask(tasks.filter((_,index)=>i!==index ));
-   
-}
-function handleStatusChange(i){
-  const updatedTasks = tasks.map((task, index) => 
-    index === i ? { ...task, status: !task.status } : task
-  );
-  setTask(updatedTasks);
-} 
+    function handleAddBtn() {
 
-     
- 
+        let x = inputref.current.value;
+        let newtask = { tak: x, status: true };
+        setTask(t => [...t, newtask]);
+        inputref.current.value = "";
+    }
+    function handleDelBtn(i) {
+        let x = 0;
+        setTask(tasks.filter((_, index) => i !== index));
 
-useEffect(()=>{
-console.log("re render");
-})
- 
- return(<>
-    <div className="todo-app">
+    }
+    function handleStatusChange(i) {
+        const updatedTasks = tasks.map((task, index) =>
+            index === i ? { ...task, status: !task.status } : task
+        );
+        setTask(updatedTasks);
+    }
+
+
+
+
+    useEffect(() => {
+        console.log("re render");
+    })
+
+    return (<>
+        <div className="todo-app">
             <h1>Todo App</h1>
             <div className="input-area">
-            <input ref={inputref} type="text" placeholder="Enter the task"></input>
-            <button onClick={handleAddBtn}>add</button>
-            </div>  <div className="task-list"> 
+                <input ref={inputref} type="text" placeholder="Enter the task"></input>
+                <button onClick={handleAddBtn}>add</button>
+            </div>
+            <div className="task-list">
                 <ul>
 
-                    {tasks.map((task,index) => (
-                        <li key={index} style={task.status? {color:"green"}:{color:"red", textDecoration: "line-through"}}>
-                           <span onClick={()=>handleDelBtn(index)}> {task.tak}</span>
-                           <span onClick={()=>handleStatusChange(index)}>{task.status ? ("pending" ): "done"}
-                           </span> 
-                            </li>
-                            ))}
+                    {tasks.map((task, index) => (
+                        <li className="list" key={index} style={task.status ? { color: "green" } : { color: "red", textDecoration: "line-through" }}>
+                            <span >
+                                <span className="status-box"onClick={() => handleStatusChange(index)}></span> 
+                                <span >  {task.tak}
+                                </span>
+                                </span>
+                            <span onClick={() => handleDelBtn(index)} >{task.status ? ("X") : "X"}
+                            </span>
+                        </li>
+                    ))}
                 </ul>
 
             </div>
